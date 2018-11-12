eb3b2d233be8d724d3fc705c1196606e8bf0e9a2
Bert Pie <BertPieGIT@gmail.com>
2018-11-12 18:38:11 +0100

    Added the quickest solution to CountZeros with reccurency

--- src/CountZeros.java
+++ src/CountZeros.java
@@ -6,7 +6,9 @@ public class CountZeros {
         System.out.println("test: " + test + " - " + zeros(test));
         test = 14;
         System.out.println("test: " + test + " - " + zeros(test));
-        test = 438;
+        test = 25;
+        System.out.println("test: " + test + " - " + zeros(test));
+        test = 125;
         System.out.println("test: " + test + " - " + zeros(test));
         test = 100000;
         System.out.println("test: " + test + " - " + zeros(test));
@@ -15,26 +17,34 @@ public class CountZeros {
 //    How many zeros depends on how many times the number was divided by 2 and 5, together they make 10.
 //    It is unnecessary to check how many times it is divided by 2, because it's always more than 5!
 
-    public static int zeros(int n) {
-        if(n<5){
+    public static int zeros(int n){
+        if(n == 0){
             return 0;
         }
-        int countDivisionsByFive = 0;
-        for (int i = 5; i <= n; i++) {
-            int temp = i;
-            boolean notDivisibleBy5 = false;
-            while (!notDivisibleBy5) {
-                if (temp % 5 == 0) {
-                    countDivisionsByFive++;
-                    temp /= 5;
-                }
-                else {
-                    notDivisibleBy5 = true;
-                }
-            }
-        }
-        return countDivisionsByFive;
+        int result = n/5;
+        return result + zeros(result);
     }
+
+//    public static int zeros(int n) {
+//        if(n<5){
+//            return 0;
+//        }
+//        int countDivisionsByFive = 0;
+//        for (int i = 5; i <= n; i++) {
+//            int temp = i;
+//            boolean notDivisibleBy5 = false;
+//            while (!notDivisibleBy5) {
+//                if (temp % 5 == 0) {
+//                    countDivisionsByFive++;
+//                    temp /= 5;
+//                }
+//                else {
+//                    notDivisibleBy5 = true;
+//                }
+//            }
+//        }
+//        return countDivisionsByFive;
+//    }
 
 /**
  * Write a program that will calculate the number of trailing zeros in a factorial of a given number.
